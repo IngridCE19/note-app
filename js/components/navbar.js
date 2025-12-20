d1b7ec65c3cb048e57cbd0a9b36a90be993cc34f
IngridCE19 <ingridce091933@gmail.com>
2025-12-20 08:26:49 -0600

    feat(UI): implement hamburger button for navbar

--- js/components/navbar.js
+++ js/components/navbar.js
@@ -1,3 +1,19 @@
 fetch("/components/navbar.html")
     .then(res => res.text())
-    .then(html => (document.getElementById("navbar").innerHTML = html));
+    .then( html => {
+        const container = document.getElementById("navbar");
+        container.innerHTML = html;
+
+        eventosNavbar();
+    });
+
+function eventosNavbar() {
+    const hamburgerBoton = document.getElementById('hamburger-btn');
+    const navMenu = document.getElementById('nav-menu');
+
+    if (hamburgerBoton && navMenu) {
+        hamburgerBoton.addEventListener('click', () => {
+            navMenu.classList.toggle('active');
+        })
+    }
+}
