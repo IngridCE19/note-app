c6843c033d5cfa3e700a05a9cfccc62b51f96d43
IngridCE19 <ingridce091933@gmail.com>
2026-01-15 16:43:21 -0600

    refactor(modals): simplify event handling for modal buttons and context menus

--- js/components/modals.js
+++ js/components/modals.js
@@ -2,45 +2,43 @@
 export function initModalEvents() {
     const modalBase = document.getElementById("modal");
 
+    function renderClick(btn, modalId) {
+        if (btn && modalId) {
+            btn.onclick = () => modalId.classList.toggle('show');
+        }
+    }
+
+    function renderContextMenu(btn, modalId) {
+        if (btn && modalId) {
+            btn.addEventListener('contextmenu', () => {
+                modalId.classList.toggle('show');
+            })
+        }   
+    }
+
     /* Boton menu para crear un nuevo archivo en index.html */
     const btnNewNote = document.getElementById('button-new-note');
     const modalNewNote = document.getElementById('container-new-note');
 
-    if (btnNewNote && modalNewNote) {
-        btnNewNote.addEventListener('click', () => {
-            modalNewNote.classList.toggle('show');
-        });
-    }
+    renderClick(btnNewNote, modalNewNote);
 
     /*Boton menu para insertar contenido multimedia en el editor*/
     const btnInsert = document.getElementById('insert-btn');
     const modalInsert = document.getElementById('container-insert');
 
-    if (btnInsert && modalInsert) {
-        btnInsert.addEventListener('click', () => {
-            modalInsert.classList.toggle('show');
-        });
-    }
+    renderClick(btnInsert, modalInsert);
 
     /*Boton menu para modificar el formato en el editor*/
     const btnFormat = document.getElementById('format-btn');
     const modalFormat = document.getElementById('container-format');
-    
-    if (btnFormat && modalFormat) {
-        btnFormat.addEventListener('click', () => {
-            modalFormat.classList.toggle('show');
-        });
-    }
+
+    renderClick(btnFormat, modalFormat);
 
     /* Click derecho para ver menu de las operaciones sobre un archivo en index.html */
     const btnCardNote = document.getElementById('note-container');
     const modalNoteOptions = document.getElementById('container-note-options');
 
-    if (btnCardNote && modalNoteOptions) {
-        btnCardNote.addEventListener('contextmenu', () => {
-            modalNoteOptions.classList.toggle('show');
-        })
-    }
+    renderContextMenu(btnCardNote, modalNoteOptions);
 
     /* Boton cerrar modal */ 
     const closeButtons = document.querySelectorAll('.close-btn');
